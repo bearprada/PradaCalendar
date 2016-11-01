76d40fdacf7d55c9c2e803e0176eaafd90dbfd37
PRADA Hsiung <bear.prada@gmail.com>
2016-11-01 20:19:54 +0800

    change the way to inject the mock data

--- app/src/main/java/example/prada/lab/pradaoutlook/CalendarActivity.java
+++ app/src/main/java/example/prada/lab/pradaoutlook/CalendarActivity.java
@@ -1,6 +1,8 @@
 package example.prada.lab.pradaoutlook;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -10,18 +12,28 @@ import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 
 import org.zakariya.stickyheaders.StickyHeaderLayoutManager;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
 
-import example.prada.lab.pradaoutlook.model.MockEventStore;
+import bolts.Continuation;
+import bolts.Task;
+import example.prada.lab.pradaoutlook.store.EventStoreFactory;
+import example.prada.lab.pradaoutlook.model.IEventDataUpdatedListener;
+import example.prada.lab.pradaoutlook.store.IEventStore;
+import example.prada.lab.pradaoutlook.model.POEvent;
 import example.prada.lab.pradaoutlook.utils.Utility;
 
-public class CalendarActivity extends AppCompatActivity implements AgendaAdapter.OnAgendaScrolledListener,
-        CompactCalendarView.CompactCalendarViewListener {
+public class CalendarActivity extends AppCompatActivity implements CompactCalendarView.CompactCalendarViewListener,
+                                                                   IEventDataUpdatedListener {
 
     private AgendaAdapter mAdapter;
     private CompactCalendarView mCalendarView;
     private RecyclerView mAgendaView;
+    private CoordinatorLayout mCoordinator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,32 +42,63 @@ public class CalendarActivity extends AppCompatActivity implements AgendaAdapter
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mCoordinator = (CoordinatorLayout) findViewById(R.id.coordinator);
         mAgendaView = (RecyclerView) findViewById(R.id.agenda_list);
         StickyHeaderLayoutManager lm = new StickyHeaderLayoutManager();
         mAgendaView.setLayoutManager(lm);
 
-        MockEventStore store = MockEventStore.getInstance(this);
+        final IEventStore store = EventStoreFactory.getInstance(this);
         Calendar from = store.getFirstEventTime();
         Calendar to = store.getLatestEventTime();
+        store.addListener(this);
 
         mAdapter = new AgendaAdapter(this, from, to);
-        mAdapter.setListener(this);
         mAgendaView.setAdapter(mAdapter);
 
         mCalendarView = (CompactCalendarView) findViewById(R.id.calendar_view);
-
         mCalendarView.setListener(this);
+
         setTitle(getTitleString(Calendar.getInstance()));
-//        mCalendarView.addEvents(new ArrayList<Event>());
+
+        // inject the test data
+        // TODO inserting the test data when the store is empty
+        Task.callInBackground(new Callable<List<POEvent>>() {
+            @Override
+            public List<POEvent> call() throws Exception {
+                java.util.Random random = new java.util.Random();
+                // generate the 200 events between last two months to now
+                final int NUM_EVENTS = 200;
+                Calendar c1 = Calendar.getInstance();
+                c1.set(Calendar.MONTH, c1.get(Calendar.MONTH) - 2);
+                Calendar c2 = Calendar.getInstance();
+                long t1 = c1.getTimeInMillis();
+                long t2 = c2.getTimeInMillis();
+                long gap = (t2 - t1) / NUM_EVENTS;
+                List<POEvent> datas = new ArrayList<>();
+                for (int i = 0; i < NUM_EVENTS ; i++) {
+                    long eventT1 = t1 + (i * gap);
+                    long eventT2 = eventT1 + (random.nextInt(3600) * 1000);
+                    datas.add(new POEvent("Event-" + random.nextInt(10000), "TODO", new Date(eventT1), new Date(eventT2)));
+                }
+                return datas;
+            }
+        }).onSuccess(new Continuation<List<POEvent>, Void>() {
+            @Override
+            public Void then(Task<List<POEvent>> task) throws Exception {
+                store.addEvents(task.getResult());
+                return null;
+            }
+        }, Task.UI_THREAD_EXECUTOR);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventStoreFactory.getInstance(this).removeListener(this);
     }
 
     private String getTitleString(Calendar calendar) {
         return calendar.get(Calendar.YEAR) + "" + Utility.convertMonthStr(calendar.get(Calendar.MONTH));
-    }
-
-    @Override
-    public void onAgendaMoveToDate(Date date) {
-        // TODO
     }
 
     @Override
@@ -67,7 +110,12 @@ public class CalendarActivity extends AppCompatActivity implements AgendaAdapter
 
     @Override
     public void onDayClick(Date dateClicked) {
-        // TODO change the agenda view
+        try {
+            // FIXME the scroll position isn't correct it might because the coordinate layout.
+            mAgendaView.smoothScrollToPosition(mAdapter.findSectionPosition(dateClicked));
+        } catch (IndexOutOfBoundsException e) {
+            Snackbar.make(mCoordinator, R.string.select_wrong_date, Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -80,5 +128,34 @@ public class CalendarActivity extends AppCompatActivity implements AgendaAdapter
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(firstDayOfNewMonth);
         setTitle(getTitleString(calendar));
+    }
+
+    @Override
+    public void onEventsInsert(Collection<POEvent> events) {
+        mAdapter.notifyDataSetChanged(); // FIXME only update the sections that's in the range of events
+        for (POEvent e : events) {
+            mCalendarView.addEvent(e.toEvent(), false);
+        }
+        mCalendarView.invalidate();
+    }
+
+    @Override
+    public void onEventInsert(POEvent event) {
+        mAdapter.updateEvent(event);
+        mCalendarView.addEvent(event.toEvent(), true);
+    }
+
+    @Override
+    public void onEventUpdate(POEvent event) {
+        mAdapter.updateEvent(event);
+        // FIXME is it the good way to implement it?
+        mCalendarView.removeEvent(event.toEvent(), false);
+        mCalendarView.addEvent(event.toEvent(), true);
+    }
+
+    @Override
+    public void onEventDelete(POEvent event) {
+        mAdapter.updateEvent(event);
+        mCalendarView.removeEvent(event.toEvent(), true);
     }
 }
